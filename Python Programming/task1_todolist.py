ef27d688756e745da1e0438040c4b20ec406c1fe
Bikash Ranjan Behera <90145679+bikash4044@users.noreply.github.com>
2024-07-12 13:47:33 +0530

    updating code

--- Python Programming/task1_todolist.py
+++ Python Programming/task1_todolist.py
@@ -10,7 +10,58 @@ users to create, update, and track their to-do lists
 # w mode: write
 
 if __name__ == "__main__":
-    open("todo.txt",'x')
-    f = open("todo.txt",'r+')
-    f.close()
+    try:
+        open("todo.txt",'x')
+    except Exception as e:
+        pass
+    l=[]
+    with open("todo.txt",'r') as f:
+        l=f.readlines()
+    
+    #CRUD
+    c='y'
+    while(c[0]=='y' or c[0]=='Y'):
+        print("1. Display the list of tasks")
+        print("2. Add a new task")
+        print("3. Delete a completed task from list")
+        inp = int(input("Enter the operation you want to perform:"))
+        if(inp==1):
+            print("List of tasks:")
+            if(len(l)>0):
+                for i in range(len(l)):
+                    print(i+1,": ",l[i])
+            else:
+                print("Empty List of task.")
+        elif(inp==2):
+            l.append(input("Enter the new task:"))
+            print("Added successfully.")
+        elif(inp==3):
+            if(len(l)==0):
+                print("Empty list.")
+            else:
+                print("List of tasks:")
+                for i in range(len(l)):
+                    print(i+1,": ",l[i])
+                d = int(input("Enter the serial no. of the task you want to remove"))
+                if(d<0 or d>len(l)):
+                    print("Invalid Input. Abroting ! ! !")
+                else:
+                    del l[d-1]
+                    print("Removed successfully.")
+        else:
+            print("Invalid Input. Aborting ! ! !")
+            exit()
+        c = input("Press Y or y to continue:")
+    with open("todo.txt",'w') as f:
+        for i in l:
+            f.write(i)
+        
+
+        
+
+    
+
+
+
+ 
     
--- Python Programming/task2_calculator.py
+++ Python Programming/task2_calculator.py
@@ -3,3 +3,23 @@ Design a simple calculator with basic arithmetic operations.
 Prompt the user to input two numbers and an operation choice.
 Perform the calculation and display the result.
 '''
+
+if __name__ == "__main__":
+    print("Calculator:")
+    a = float(input("Enter the first number: "))
+    b = float(input("Enter the second number: "))
+    print("Avaialable calculations:\n1. Addition (+)\n2. Subtraction (-)\n3. Multiplication (x)\n4. Division (/)")
+    c = int(input("Enter your choice: "))
+    if(c==4 and b==0):
+        print("Error: Division by zero not possible. Aborting ! ! !")
+        exit()
+    if(c==1):
+        print("The answer is ",a+b)
+    elif(c==2):
+        print("The answer is ",a-b)
+    elif(c==3):
+        print("The answer is ",a*b)
+    elif(c==4):
+        print("The answer is ",a/b)
+        if((int(a)-a)==0 and (int(b)-b)==0):
+            print("Integer Division possible: Quotient= ",int(a)//int(b)," Remainder= ",int(a)%int(b))
