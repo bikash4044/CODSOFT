cb53c42265953ed3bd1ff6cb97253a44072ade4b
Bikash Ranjan Behera <90145679+bikash4044@users.noreply.github.com>
2024-07-12 14:05:54 +0530

    Update task4_rockpaperscissor.py

--- Python Programming/task4_rockpaperscissor.py
+++ Python Programming/task4_rockpaperscissor.py
@@ -13,3 +13,21 @@ Play Again: Ask the user if they want to play another round.
 User Interface: Design a user-friendly interface with clear instructions and
 feedback.
 '''
+
+import random
+Result=[["Match Draw","You Win","You loose"],["You loose","Match Draw","You Win"],["You Win","You loose","Match Draw"]]
+print("Rock, Paper, Scissor")
+usrin = input("Enter your choice:")
+if(usrin[0]=='R' or usrin[0]=='r'):
+    usr=0
+if(usrin[0]=='P' or usrin[0]=='p'):
+    usr=1
+if(usrin[0]=='S' or usrin[0]=='s'):
+    usr=2
+
+ls = ["Rock","Paper","Scissor"]
+print(f"You have selected {ls[usr]}")
+comp = random.randint(0,2)
+print(f"Computer has selected {ls[comp]}")
+
+print(Result[comp][usr])
