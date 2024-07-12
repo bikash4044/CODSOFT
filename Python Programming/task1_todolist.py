e784c91835484c03000028db9f2d9f430a1b5149
Bikash Ranjan Behera <90145679+bikash4044@users.noreply.github.com>
2024-07-12 12:34:40 +0530

    Update task1_todolist.py

--- Python Programming/task1_todolist.py
+++ Python Programming/task1_todolist.py
@@ -4,3 +4,13 @@ and organize their tasks efficiently. This project aims to create a
 command-line or GUI-based application using Python, allowing
 users to create, update, and track their to-do lists
 '''
+
+# x mode: if not exists
+# r mode: read
+# w mode: write
+
+if __name__ == "__main__":
+    open("todo.txt",'x')
+    f = open("todo.txt",'r+')
+    f.close()
+    
