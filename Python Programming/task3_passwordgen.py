ac2719e608e94f830d5fe640f7f717a69264531a
Bikash Ranjan Behera <90145679+bikash4044@users.noreply.github.com>
2024-07-13 20:52:28 +0530

    second last

--- Python Programming/task3_passwordgen.py
+++ Python Programming/task3_passwordgen.py
@@ -10,3 +10,20 @@ Generate Password: Use a combination of random characters to
 generate a password of the specified length.
 Display the Password: Print the generated password on the screen.
 '''
+import random
+
+l = int(input("Enter the length of password:"))
+if(l<4):
+    print("Length must be 4 at least. Aborting ! ! !")
+    exit()
+pswd=[]
+for i in range(l-2):
+    pswd.append(chr(random.randint(97,122)))
+for i in range(2):
+    x = random.randint(0,l-2-1)
+    pswd[x]= pswd[x].upper()
+for i in range(2):
+    loc = random.randint(0,l+i-1)
+    pswd.insert(loc,chr(random.randint(33,64)))
+pswd=''.join(pswd)
+print(f"Password: {pswd}")
--- Python Programming/task5_contactbook.py
+++ Python Programming/task5_contactbook.py
@@ -7,3 +7,72 @@ Update Contact: Enable users to update contact details.
 Delete Contact: Provide an option to delete a contact.
 User Interface: Design a user-friendly interface for easy interaction.
 '''
+
+def add():
+    pass
+
+def search():
+    pass
+
+def view():
+    for i in range(len(l)):
+        print(f"{i+1}. {l[i][0]} : {l[i][1]}")
+
+def update():
+    pass
+
+def delete():
+    view()
+    i = int(input("Enter the serial number of contact to be deleted:"))
+    if(i>len(l)):
+        print("Invalid Input ! ! !")
+    else:
+        del l[i-1]
+    print("Deleted successfully.")
+
+
+
+
+
+if __name__ == "__main__":
+    try:
+        open("todo.txt",'x')
+    except Exception as e:
+        pass
+    l=[]
+
+    #use file handling to store the details
+    with open("todo.txt",'r') as f:
+        tmp=[]
+        for i in range(4):
+            tmp.append(f.readlines())
+        l.insert(len(l),tmp)
+
+    c='y'
+    while(c[0]=='y' or c[0]=='Y'):
+        print("1. Add new contact")
+        print("2. View contact list")
+        print("3. Search for a contact")
+        print("4. Update a contact")
+        print("5. Delete a contact")
+        inp = int(input("Enter the operation you want to perform:"))
+        if(inp==1):
+            add()
+        elif(inp==2):
+            view()
+        elif(inp==3):
+            search()
+        elif(4):
+            update()
+        elif(5):
+            if(len(l)==0):
+                print("Empty list.")
+            else:
+                delete()
+        else:
+            print("Invalid Input. Aborting ! ! !")
+            exit()
+        c = input("Press Y or y to continue:")
+    with open("todo.txt",'w') as f:
+        for i in l:
+            f.write(i)
